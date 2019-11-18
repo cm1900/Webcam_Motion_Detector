af4191f7d45354b92a5cd7cbd386f1d1908139cb
cm1900 <cmalebe139@gmail.com>
2019-11-18 13:07:56 +0200

    Find contours and draw rectangle on image

--- main.py
+++ main.py
@@ -15,12 +15,23 @@ while True:
 
     delta_frame = cv2.absdiff(initial_frame, gray)
 
-    thresh_delta = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    threshold_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+
+    threshold_frame = cv2.dilate(threshold_frame, None, iterations= 2)
+    (cnts,_) = cv2.findContours(threshold_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
     cv2.imshow("Capturing", frame)
-    cv2.imshow("Delta Image", delta_frame)
+    #cv2.imshow("Delta Image", delta_frame)
+    cv2.imshow("Threshold Frame", threshold_frame)
+
     key = cv2.waitKey(1)
-    if key == ord('w'):
+    if key == ord('q'):
         break
 video.release()
 cv2.destroyAllWindows()
