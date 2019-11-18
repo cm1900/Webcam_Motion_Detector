0add80a89bb40071948fbb844853f8a875abe0bd
cm1900 <cmalebe139@gmail.com>
2019-11-18 12:08:52 +0200

    Store the initial frame

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import cv2
 
+initial_frame = None
+
 video = cv2.VideoCapture(0)
 while True:
     check, frame = video.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if initial_frame is None:
+        initial_frame = gray
+        continue
 
     cv2.imshow("Capturing", frame)
     key = cv2.waitKey(1)
